058b873d1a2466224470e049b7ce0b28293b6194
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-03 12:33:12 -0400

    get forums endpoint

--- src/routes/forums.ts
+++ src/routes/forums.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import forumsRouter from "./forums/[forumID].js";
+import database from "#utils/database-generator.js";
 
 const router = Router();
 
 router.use("/forums", forumsRouter);
+
+router.get("/", async (req, res) => {
+  try {
+    const docs = await database.collection("forums").find().toArray();
+    res.json(docs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 export default router;
