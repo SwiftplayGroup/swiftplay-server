fee5d51fcf55c4fdc64e2c5a0a9521424e841cab
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-10 23:57:33 -0400

    i think this will fix the issues (#134)

--- src/utils/database-generator.ts
+++ src/utils/database-generator.ts
@@ -3,13 +3,8 @@ import { MongoClient } from "mongodb";
 const mongoURI = process.env.MONGODB_URI;
 if (!mongoURI) throw new Error("Please define MONGODB_URI in the .env file.");
 
-const isProd = process.env.NODE_ENV === "production";
-const certPath = isProd
-  ? "/etc/secrets/mongodb.pem"
-  : "./etc/secrets/mongodb.pem";
-
 const mongoClient = new MongoClient(mongoURI, {
-  tlsCertificateKeyFile: certPath,
+  tlsCertificateKeyFile: "/etc/secrets/mongodb.pem",
 });
 const database = mongoClient.db(process.env.MONGODB_DATABASE);
 
