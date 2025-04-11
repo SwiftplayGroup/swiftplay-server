d4a1ff6ea96ce71c771b8a81a71fc78ee48ae049
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-10 21:53:55 -0400

    inti (#122)

--- src/utils/database-generator.ts
+++ src/utils/database-generator.ts
@@ -3,8 +3,13 @@ import { MongoClient } from "mongodb";
 const mongoURI = process.env.MONGODB_URI;
 if (!mongoURI) throw new Error("Please define MONGODB_URI in the .env file.");
 
+const isProd = process.env.NODE_ENV === "production";
+const certPath = isProd
+  ? "/etc/secrets/mongodb.pem"
+  : "./etc/secrets/mongodb.pem";
+
 const mongoClient = new MongoClient(mongoURI, {
-  tlsCertificateKeyFile: "./etc/secrets/mongodb.pem",
+  tlsCertificateKeyFile: certPath,
 });
 const database = mongoClient.db(process.env.MONGODB_DATABASE);
 
