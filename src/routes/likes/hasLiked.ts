f630c8a910df7c12e35975932921b5fbba5f2bad
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-12 12:03:49 -0400

    fix has liked to pull from req query not params (#138)

--- src/routes/likes/hasLiked.ts
+++ src/routes/likes/hasLiked.ts
@@ -3,27 +3,19 @@ import database from "#utils/database-generator.js";
 
 const router = Router();
 //has someone liked a specific post.
-// GET /likes/hasLiked?userId=123&postId=456
-router.get(
-  "/",
-  async (req: Request<{ userId: string; postId: string }>, res) => {
-    const { userId, postId } = req.query;
+router.get("/", async (req: Request, res) => {
+  const { userId, postId } = req.query;
 
-    if (!userId || !postId) {
-      return res.status(400).json({ error: "Missing userId or postId" });
-    }
+  if (typeof userId !== "string" || typeof postId !== "string") {
+    return res.status(400).json({ error: "Missing userId or postId" });
+  }
 
-    try {
-      const like = await database
-        .collection("likes")
-        .findOne({ userId, postId });
+  try {
+    const like = await database.collection("likes").findOne({ userId, postId });
 
-      res.json({ liked: !!like });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Server error" });
-    }
-  },
-);
-
-export default router;
+    res.json({ liked: !!like });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
