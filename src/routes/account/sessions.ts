e295588d57889ac25fddfb3713328d30a6b95d0a
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-11 21:28:02 -0400

    add the by-token endpoint to fetch by the hashed token.

--- src/routes/account/sessions.ts
+++ src/routes/account/sessions.ts
@@ -3,46 +3,39 @@ import { Router } from "express";
 import { hash as hashString, verify as verifyPassword } from "argon2";
 import { randomBytes } from "crypto";
 import sessionIDRouter from "./sessions/[sessionID].js";
+import byTokenRouter from "./sessions/by-token.js";
 
-const router = Router({mergeParams: true});
+const router = Router({ mergeParams: true });
 router.use("/:sessionID", sessionIDRouter);
+router.use("/by-token", byTokenRouter);
 
 router.post("/", async (request, response) => {
-
   // Verify that the user provides a valid username and password.
-  const {username, password} = request.body;
-  if (typeof(username) !== "string") {
-
+  const { username, password } = request.body;
+  if (typeof username !== "string") {
     return response.status(400).json({
-      message: "Username must be a string."
+      message: "Username must be a string.",
     });
-
-  } else if (typeof(password) !== "string") {
-
+  } else if (typeof password !== "string") {
     return response.status(400).json({
-      message: "Password must be a string."
+      message: "Password must be a string.",
     });
-
   } else if (!username.trim() || !password) {
-
     return response.status(400).json({
-      message: `A ${username ? "password" : "username"} is required.`
+      message: `A ${username ? "password" : "username"} is required.`,
     });
-
   }
 
   const accountsCollection = database.collection("accounts");
   const userFilter = {
-    username: new RegExp(`^${username}$`, "i")
+    username: new RegExp(`^${username}$`, "i"),
   };
   const userData = await accountsCollection.findOne(userFilter);
 
-  if (!(userData && await verifyPassword(userData.password, password))) {
-
+  if (!(userData && (await verifyPassword(userData.password, password)))) {
     return response.status(401).json({
-      message: "Incorrect username or password."
+      message: "Incorrect username or password.",
     });
-
   }
 
   // Create a random hashed token and save it to the user's profile in the database.
@@ -54,30 +47,29 @@ router.post("/", async (request, response) => {
     creationDate,
     creationIP: request.socket.remoteAddress,
     expirationDate,
-    accountID: userData._id
+    accountID: userData._id,
   };
 
   let sessionID;
 
   try {
-    
-    const { insertedId } = await database.collection("sessions").insertOne({...sessionData, tokenHash: await hashString(sessionToken)});
+    const { insertedId } = await database
+      .collection("sessions")
+      .insertOne({ ...sessionData, tokenHash: await hashString(sessionToken) });
 
     sessionID = insertedId;
-
   } catch (error: unknown) {
-
     console.error(error);
 
     return response.status(500).json({
-      message: "Something bad happened on our side. Try again later."
+      message: "Something bad happened on our side. Try again later.",
     });
-
   }
 
   // Return a 201 success, and a JSON response body with the session data.
-  return response.status(201).json({...sessionData, token: sessionToken, sessionID});
-
+  return response
+    .status(201)
+    .json({ ...sessionData, token: sessionToken, sessionID });
 });
 
 export default router;
