1bc9695d6b23771d425dc5649ed0577322e31772
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-11 21:28:30 -0400

    add the by-token endpoint to fetch by the hashed token. (#135)

--- src/routes/account/sessions/[sessionID].ts
+++ src/routes/account/sessions/[sessionID].ts
@@ -3,62 +3,52 @@ import { Request, Router } from "express";
 import authenticator from "#utils/authenticator.js";
 import { ObjectId } from "mongodb";
 
-const router = Router({mergeParams: true});
+const router = Router({ mergeParams: true });
 
 router.delete("/", authenticator);
-router.delete("/", async (request: Request<{ sessionID: string }>, response) => {
+router.delete(
+  "/",
+  async (request: Request<{ sessionID: string }>, response) => {
+    // Verify that the user provides a valid session ID in the body.
+    const sessionIDString = request.params.sessionID;
+    let sessionID;
+    const sessionsCollection = database.collection("sessions");
+    let sessionFilter;
+    try {
+      sessionID = new ObjectId(sessionIDString);
+      sessionFilter = {
+        _id: sessionID,
+        accountID: response.locals.accountData._id,
+      };
 
-  // Verify that the user provides a valid session ID in the body.
-  const sessionIDString = request.params.sessionID;
-  let sessionID;
-  const sessionsCollection = database.collection("sessions");
-  let sessionFilter;
-  try {
+      if ((await sessionsCollection.countDocuments(sessionFilter)) == 0) {
+        throw new Error();
+      }
+    } catch (error: unknown) {
+      console.error(error);
 
-    sessionID = new ObjectId(sessionIDString);
-    sessionFilter = {
-      _id: sessionID, 
-      accountID: response.locals.accountData._id
-    };
-
-    if (await sessionsCollection.countDocuments(sessionFilter) == 0) {
-
-      throw new Error();
-
+      return response.status(404).json({
+        message: "Session not found.",
+      });
     }
 
-  } catch (error: unknown) {
+    try {
+      const { deletedCount } =
+        await sessionsCollection.deleteOne(sessionFilter);
 
-    console.error(error);
+      if (deletedCount === 0) {
+        throw new Error("Unable to delete session.");
+      }
+    } catch (error) {
+      console.error(error);
 
-    return response.status(404).json({
-      message: "Session not found."
-    });
-
-  }
-
-  try {
-
-    const { deletedCount } = await sessionsCollection.deleteOne(sessionFilter);
-
-    if (deletedCount === 0) {
-
-      throw new Error("Unable to delete session.");
-
+      return response.status(500).json({
+        message: "Something bad happened on our side. Try again later.",
+      });
     }
 
-  } catch (error) {
-
-    console.error(error);
-
-    return response.status(500).json({
-      message: "Something bad happened on our side. Try again later."
-    });
-
-  }
-
-  return response.status(204).json({});
-
-});
+    return response.status(204).json({});
+  },
+);
 
 export default router;
