fa9c2a6df890d362d6f8f5734cc1ad3da235d5dd
Christian Toney <christian.toney@beastslash.com>
2025-04-12 23:41:58 -0400

    Add feature: Delete run category

--- src/routes/game-pages/[gamePageID]/categories/[categoryID].ts
+++ src/routes/game-pages/[gamePageID]/categories/[categoryID].ts
@@ -1,3 +1,4 @@
+import addToAuditLog from "#utils/addToAuditLog.js";
 import authenticator, { defaultPermissions } from "#utils/authenticator.js";
 import database from "#utils/database-generator.js";
 import { Request, Router } from "express";
@@ -5,9 +6,9 @@ import { ObjectId } from "mongodb";
 
 const router = Router({ mergeParams: true });
 
-// Create a run category.
+// Delete a run category.
 router.delete("/", authenticator);
-router.delete("/", async (request: Request<{ gamePageID: string, categoryID: string }>, response) => {
+router.delete("/", async (request: Request<{ categoryID: string }>, response) => {
 
   // Verify permissions.
   // TODO: Check game page permissions.
@@ -20,11 +21,79 @@ router.delete("/", async (request: Request<{ gamePageID: string, categoryID: str
 
   }
 
-  console.log(`Successfully created run category: `);
+  let category;
+  const runCategoriesCollection = database.collection("runCategories");
 
-  return response.status(204).json({
-    success: true
-  })
+  try {
+    
+    const gamePageID = new ObjectId(request.params.categoryID);
+    category = await runCategoriesCollection.findOne({
+      _id: new ObjectId(gamePageID)
+    });
+
+    if (!category) {
+
+      return response.status(404).json({
+        message: "Run category not found.",
+      });
+
+    }
+
+  } catch (error: unknown) {
+
+    if (error instanceof Error && error.name.slice(0, 9) === "BSONError") {
+
+      return response.status(404).json({
+        message: "Run category not found.",
+      });
+
+    } else {
+
+      console.log(error);
+
+      return response.status(500).json({
+        message: "Something bad happened on our side. Try again later.",
+      });
+
+    }
+    
+  }
+
+  try {
+
+    // Move all runs to the default category.
+    await database.collection("runs").updateMany({
+      categoryID: category._id
+    }, {
+      $unset: {
+        categoryID: 1
+      }
+    });
+    
+    console.log(`Moved all runs associated with ${category._id} to the default category`);
+
+    // Delete the run category from the records.
+    await runCategoriesCollection.deleteOne({
+      _id: category._id
+    });
+
+    console.log(`Successfully deleted a run category: ${category._id}`);
+
+    await addToAuditLog("gamePages.categories.delete", actorID, category._id, response.locals.sessionID);
+
+    return response.status(204).json({
+      success: true
+    });
+
+  } catch (error: unknown) {
+
+    console.warn(error);
+
+    return response.status(500).json({
+      message: "Something bad happened on our side. Try again later.",
+    });
+
+  }
 
 });
 
