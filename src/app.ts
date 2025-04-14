8f924b61911f392346962dd73855d859f74cc637
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-14 19:39:10 -0400

    new endpoints

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import groupsRouter from "./routes/groups/index.js";
 import forumsRouter from "./routes/forums/index.js";
 import threadsRouter from "./routes/threads/index.js";
 import likesRouter from "./routes/likes/index.js";
+import postsRouter from "./routes/posts/index.js";
+import forumIdRouter from "./routes/forums/[forumID]/index.js";
 import cors from "cors";
 
 const app = express();
@@ -16,9 +18,11 @@ app.disable("x-powered-by");
 app.use("/runs", runsRouter);
 app.use("/account", accountRouter);
 app.use("/accounts", accountsRouter);
+app.use("/posts", postsRouter);
 app.use("/game-pages", gamePagesRouter);
 app.use("/groups", groupsRouter);
 app.use("/forums", forumsRouter);
+app.use("/forums/:forumId", forumIdRouter);
 app.use("/threads", threadsRouter);
 app.use("/likes", likesRouter);
 
--- src/routes/forums/[forumID]/index.ts
+++ src/routes/forums/[forumID]/index.ts
@@ -8,11 +8,21 @@ const router = Router({
 
 router.get("/", async (req: Request<{ forumID: string }>, res) => {
   const forumID = req.params.forumID;
+  console.log(forumID);
   const forum = await database
     .collection("forums")
     .findOne({ _id: new ObjectId(forumID) });
   if (!forum) return res.status(404).send("Forum not found");
   res.send(forum);
+});
+
+router.get("/threads", async (req: Request<{ forumID: string }>, res) => {
+  const forumID = req.params.forumID;
+  const threads = await database
+    .collection("threads")
+    .find({ forumID })
+    .toArray();
+  res.send(threads);
 });
 
 export default router;
