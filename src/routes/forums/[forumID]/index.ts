bba0509912ecadec8900846b8703e6ed37c94fd9
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-14 19:51:18 -0400

    finish endpoint

--- src/routes/forums/[forumID]/index.ts
+++ src/routes/forums/[forumID]/index.ts
@@ -20,7 +20,7 @@ router.get("/threads", async (req: Request<{ forumID: string }>, res) => {
   const forumID = req.params.forumID;
   const threads = await database
     .collection("threads")
-    .find({ forumID })
+    .find({ forum: forumID })
     .toArray();
   res.send(threads);
 });
