84628caf6be62d1bd89d81695f2009a852f787d4
Michael Strange <66543114+michael-strange@users.noreply.github.com>
2025-04-15 23:19:13 -0400

    Route fix (#175)
    * Fixed forums/[forumID]/posts/get.ts route to handle threads_only properly
    
    * type check
    
    ---------
    
    Co-authored-by: Christian Toney <christian.toney@beastslash.com>

--- src/routes/forums/[forumID]/posts/get.ts
+++ src/routes/forums/[forumID]/posts/get.ts
@@ -7,9 +7,38 @@ const getPostsRouter = Router({
 
 getPostsRouter.get("/", async (req: Request<{ forumID: string }>, res) => {
   const forumID = req.params.forumID;
+  const type = req.query.type;
+  let filter = {};
+  if (type === "post") {
+
+    filter = {
+      parentPostID: {
+        $ne: null
+      }
+    };
+
+  } else if (type === "thread") {
+
+    filter = {
+      parentPostID: null
+    };
+
+  } else if (type !== undefined) {
+
+    return res.status(400).json({
+      message: `Type must be empty, "post", or "thread".`
+    });
+
+  }
+
+  const query = { 
+    forumID,
+    ...filter
+  };
+  
   const posts = await database
     .collection("posts")
-    .find({ forumID, ...(req.query.threads_only === "true" ? {parentPostID: null} : {}) })
+    .find(query)
     .toArray();
   res.json(posts);
 });
