a23ebbef9d8d7115472993c3f5962621b4de30dc
Christian Toney <christian.toney@beastslash.com>
2025-04-17 00:15:51 -0400

    fix game page

--- src/routes/game-pages/[gamePageID]/get.ts
+++ src/routes/game-pages/[gamePageID]/get.ts
@@ -11,9 +11,10 @@ getGamePageRouter.get("/", async (request: Request<{ gamePageID: string }>, resp
     objectID = new ObjectId(request.params.gamePageID);
   } catch (error: unknown) {
     console.log(error);
-    return response.status(404).json({
+    response.status(404).json({
       message: "Game page not found.",
     });
+    return;
   }
 
   const document = await database.collection("gamePages").findOne({
@@ -21,9 +22,10 @@ getGamePageRouter.get("/", async (request: Request<{ gamePageID: string }>, resp
   });
 
   if (!document) {
-    return response.status(404).json({
+    response.status(404).json({
       message: "Game page not found.",
     });
+    return;
   }
 
   // Rename sensitive keys.
@@ -37,7 +39,7 @@ getGamePageRouter.get("/", async (request: Request<{ gamePageID: string }>, resp
     page[newKey] = document[key];
   }
 
-  return response.json(page);
+  response.json(page);
 });
 
 export default getGamePageRouter;
