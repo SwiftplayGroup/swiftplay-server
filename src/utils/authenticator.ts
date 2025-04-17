39e0ede52cfa1083716e2c19af3276f101591828
Christian Toney <christian.toney@beastslash.com>
2025-04-17 00:39:20 -0400

    add support for httponly cookies

--- src/utils/authenticator.ts
+++ src/utils/authenticator.ts
@@ -8,20 +8,20 @@ async function authenticator(request: Request, response: Response, next: NextFun
 
   try {
 
-    const token = request.headers.token ?? request.cookies;
-    const accountIDString = request.headers["account-id"];
+    const token = request.headers.token ?? request.cookies.sessionToken;
+    const userIDString = request.headers["account-id"] ?? request.cookies.userID;
 
-    if (typeof(token) == "string" && typeof(accountIDString) == "string") {
+    if (typeof(token) == "string" && typeof(userIDString) == "string") {
 
-      const accountID = new ObjectId(accountIDString);
-      const sessions = await database.collection("sessions").find({accountID}).toArray();
+      const userID = new ObjectId(userIDString);
+      const sessions = await database.collection("sessions").find({userID}).toArray();
 
       for (const session of sessions) {
 
         if (await verify(session.tokenHash, token)) {
 
           // Save account data.
-          const user = await User.getFromID(accountID);
+          const user = await User.getFromID(userID);
           user.setSessionID(session._id);
           response.locals.user = user;
 
