de5457f8d3d494aa2a9fc8d57068e8bf5261b836
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-17 20:59:09 -0400

    define the endpoints for calling the vector db

---
+++ src/routes/threads/recommended/[userID]/get.ts
@@ -0,0 +1,40 @@
+import { Router, Request } from "express";
+import { InternalServerError } from "#classes/errors/InternalServerError.js";
+import database from "#utils/database-generator.js";
+
+const getRecommendedThreadsRouter = Router({
+  mergeParams: true,
+});
+
+getRecommendedThreadsRouter.get(
+  "/",
+  async (req: Request<{ userID: string }>, res) => {
+    try {
+      const userID = req.params.userID;
+      const user = await database.collection("users").findOne({ _id: userID });
+      if (!user) {
+        throw new InternalServerError("User not found");
+      }
+      const userEmbeddings = user.embeddings;
+      const recommendedThreads = await database
+        .collection("threads")
+        .aggregate([
+          {
+            $vectorSearch: {
+              index: "RecommendedThreads",
+              path: "embeddings",
+              queryVector: userEmbeddings,
+              numCandidates: 100,
+              limit: 10,
+              similarity: "cosine",
+            },
+          },
+        ])
+        .toArray();
+      res.status(200).json({ recommendedThreads });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+);
