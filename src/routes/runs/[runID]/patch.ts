5072e2d649aee8347b3304aecd6f0d666d54d11a
Christian Toney <christian.toney@beastslash.com>
2025-04-19 18:43:33 -0400

    Add verification check

--- src/routes/runs/[runID]/patch.ts
+++ src/routes/runs/[runID]/patch.ts
@@ -7,6 +7,7 @@ import { BadRequestError } from "#classes/errors/BadRequestError.js";
 import { RunNotFoundError } from "#classes/errors/RunNotFoundError.js";
 import { NoPermissionError } from "#classes/errors/NoPermissionError.js";
 import { InternalServerError } from "#classes/errors/InternalServerError.js";
+import Permission, { PermissionAccessLevel } from "#classes/Permission.js";
 
 const editRunRouter = Router({ mergeParams: true });
 
@@ -22,36 +23,86 @@ editRunRouter.patch("/", async (request: Request<{ runID: string }>, response) =
     const unsetProperties: {[key: string]: 1} = {};
     for (const key of Object.keys(request.body)) {
 
-      const youtubeRegex = /^[^"&?\/\s]{11}$/gi;
+      const youtubeRegex = /^[^"&?/\s]{11}$/gi;
       const keyChecks: {[key: string]: (value: unknown) => unknown} = {
-        verification: (value: unknown) => {
+        verification: async (verification: unknown) => {
 
           // Make sure the user can do this.
+          const verifyRunsPermission = await Permission.getFromHierarchicalName("games.runs.verify");
+          actor.verifyPermission(verifyRunsPermission, PermissionAccessLevel.USER);
 
+          if (verification === null) {
 
-          if (value === null) {
-
-            delete request.body.verification;
             unsetProperties.verification = 1;
 
-          } else if (typeof(value) === "object" && !(value instanceof Array)) {
+          } else if (typeof(verification) === "object" && !(verification instanceof Array)) {
 
-            for (const key of Object.keys(value)) {
+            const validatedVerification: Record<string, unknown> = {};
 
-              const validations = {
-                ownerID: (value: unknown) => typeof(value) === "string",
-                // TODO: Turn verifications into 
+            for (const property of Object.keys(verification)) {
+
+              const validators: {[property: string]: (property: unknown) => unknown} = {
+                ownerID: async (ownerID: unknown) => {
+
+                  // Verify user exists.
+                  if (typeof(ownerID) !== "string") {
+
+                    throw new BadRequestError("ownerID must be a user ID.");
+
+                  }
+
+                  const owner = await User.getFromID(ownerID);
+
+                  return owner._id;
+
+                }
+              };
+
+              if (!(property in validators)) {
+
+                throw new BadRequestError(`${key} isn't a valid verification property.`);
+
               }
+
+              const validator = validators[property];
+              const validatedValue = await validator(verification[property as keyof typeof verification]);
+              validatedVerification[property] = validatedValue;
 
             }
 
+            validatedVerification.timestamp = new Date();
+
+            return validatedVerification;
+
+          } else {
+
+            throw new BadRequestError("Verification must be null or an object.");
+
           }
 
-          throw new BadRequestError("Verification must be null or an object.");
-
         },
-        durationMilliseconds: (value: unknown) => typeof (value) === "number",
-        youtubeWatchID: (value: unknown) => typeof (value) === "string" && youtubeRegex.test(value)
+        durationMilliseconds: (value: unknown) => {
+          
+          if (typeof (value) !== "number") {
+
+            throw new BadRequestError("durationMilliseconds must be a number.");
+
+          }
+
+          return value;
+            
+        },
+        youtubeWatchID: (value: unknown) => {
+          
+          if (typeof (value) !== "string" || !youtubeRegex.test(value)) {
+
+            throw new BadRequestError("youtubeWatchID must be a YouTube watch ID.");
+
+          }
+
+          return value;
+
+        }
       };
 
       const keyCheck = keyChecks[key];
@@ -71,6 +122,8 @@ editRunRouter.patch("/", async (request: Request<{ runID: string }>, response) =
 
     }
 
+    console.log(request.body);
+
     targetRun = await targetRun.edit(
       {
         $set: request.body,
@@ -80,7 +133,7 @@ editRunRouter.patch("/", async (request: Request<{ runID: string }>, response) =
 
     await addToAuditLog("games.runs.edit", actor._id, targetRun._id, response.locals.sessionID);
 
-    response.status(200).json(targetRun);
+    response.status(200).json(await targetRun.getExtendedProperties());
 
   } catch (error: unknown) {
 
